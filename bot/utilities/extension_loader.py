8aa6a98df17fb084a84bcb31befcaa5f991362b5
мекатів <106783764+meqativ@users.noreply.github.com>
2024-11-29 02:33:25 +0200

    Update extension_loader.py

--- bot/utilities/extension_loader.py
+++ bot/utilities/extension_loader.py
@@ -9,7 +9,8 @@ def load_extensions(client: interactions.Client, unload: bool=False, print=print
 
 	files = [f for f in os.listdir('bot/extensions') if f != '__pycache__']
 	extensions = [f.replace('.py', '') for f in files]
-
+	extensions = [None if len(f) < 0 else f for f in extensions]
+	
 	if not get_config("music.enabled") and 'music' in extensions:
 		extensions.remove("music")
 
@@ -21,6 +22,8 @@ def load_extensions(client: interactions.Client, unload: bool=False, print=print
 	else:
 		print(("Loading extensions" if not unload else "Reloading extensions") + " ... \033[s", flush=True)
 	for ext in extensions:
+		if not ext:
+			continue
 		if unload: 
 			client.unload_extension(f"extensions.{ext}")
 		if debugging():
