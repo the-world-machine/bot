d94de28bf8b19605570308a3550ec5c6bfd1ad7a
meqativ <meqativ@users.noreply.github.com>
2025-03-07 22:31:44 +0200

    rename temporaries to minis

--- bot/extensions/commands/wool.py
+++ bot/extensions/commands/wool.py
@@ -4,7 +4,7 @@ from interactions import *
 from utilities.emojis import emojis, make_url
 from datetime import datetime, timedelta
 from utilities.database.schemas import UserData
-from utilities.localization import Localization, fnum, temporary_notip
+from utilities.localization import Localization, fnum, put_mini
 from utilities.message_decorations import Colors, fancy_message
 # yapf: disable
 wool_finds = {
@@ -91,7 +91,7 @@ class WoolCommands(Extension):
 			confirmation_m = await fancy_message(
 			    ctx,
 			    message=loc.l("wool.transfer.to.bot.confirmation") +
-			    await temporary_notip(loc, ctx.user.id, "wool.transfer.to.bot.notefirmation", "note", "\n\n"),
+			    await put_mini(loc, ctx.user.id, "wool.transfer.to.bot.notefirmation", "note", "\n\n"),
 			    color=Colors.WARN,
 			    components=buttons,
 			    ephemeral=True
@@ -113,7 +113,7 @@ class WoolCommands(Extension):
 			return await fancy_message(
 			    ctx,
 			    loc.l("wool.transfer.errors.not_enough", balance=fnum(from_user.wool, locale=ctx.locale)) +
-			    await temporary_notip(loc, ctx.user.id, "wool.transfer.errors.note_nuf", "note", "\n\n"),
+			    await put_mini(loc, ctx.user.id, "wool.transfer.errors.note_nuf", "note", "\n\n"),
 			    edit=True,
 			    ephemeral=True,
 			    color=Colors.BAD
