8b718969b6e430d81b3a392dc10f9db59417c869
meqativ <meqativ@users.noreply.github.com>
2025-05-04 02:56:37 +0300

    feat(logging): add configurable log level

--- src/utilities/logging.py
+++ src/utilities/logging.py
@@ -1,3 +1,4 @@
+from utilities.config import debugging, get_config
 import logging
 import time
 
@@ -10,18 +11,19 @@ class IgnoreFilter(logging.Filter):
 		return not any(ignored_msg in message.getMessage() for ignored_msg in self.ignored)
 
 ignored = [
-	"❤ Gateway is sending a Heartbeat",
-	"❤ Received heartbeat acknowledgement from gateway",
-	"Sending data to websocket: {\"op\": 1, \"d\": 18}"
+    "❤ Gateway is sending a Heartbeat", "❤ Received heartbeat acknowledgement from gateway",
+    "Sending data to websocket: {\"op\": 1, \"d\": 18}"
 ]
+
+loggingLevel = logging.getLevelName(get_config(f"bot{"" if debugging() else ".prod"}.logging-level").upper())
 def createLogger(name):
 	logger = logging.getLogger(name)
-	logger.setLevel(logging.DEBUG)
+	logger.setLevel(loggingLevel)
 
 	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 	for handler in [logging.FileHandler(f'logs/{time.strftime("%Y.%m.%d")}.log', mode="a", encoding='utf-8'), logging.StreamHandler()]:
-		handler.setLevel(logging.DEBUG)
+		handler.setLevel(loggingLevel)
 		handler.addFilter(IgnoreFilter(ignored))
 		handler.setFormatter(formatter)
 		logger.addHandler(handler)
