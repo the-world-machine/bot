2daade8f408c23e38d9ac8c4945c964f1519fe29
meqativ <meqativ@users.noreply.github.com>
2025-08-04 02:45:38 +0300

    feat(about): parse description into buttons & rearrange locstrings

--- src/extensions/commands/about.py
+++ src/extensions/commands/about.py
@@ -1,17 +1,17 @@
 import psutil
 import platform
-from datetime import datetime
+from datetime import datetime, timezone
 from utilities.misc import get_git_hash
 from utilities.localization import Localization, fnum, ftime
 from utilities.message_decorations import Colors, fancy_message
-from interactions import Embed, Extension, Message, OptionType, SlashContext, contexts, integration_types, slash_command, slash_option
-
+from interactions import ActionRow, Button, ButtonStyle, Embed, EmbedField, Extension, Message, OptionType, SlashContext, contexts, integration_types, slash_command, slash_option
 
 try:
 	commit_hash = get_git_hash()
 	print(f"Found git hash: {commit_hash}")
 except Exception as e:
 	print(f"Error retrieving git hash: {e}")
+
 
 class AboutCommand(Extension):
 	@slash_command(description='About the bot (ping, stats)')
@@ -24,58 +24,74 @@ class AboutCommand(Extension):
 	@contexts(bot_dm=True)
 	async def about(self, ctx: SlashContext, public: bool = False):
 		loc = Localization(ctx.locale)
-		checkpoint = datetime.now()  # timezone :aware: date
+		start_time = datetime.now(timezone.utc)
 		_ = await fancy_message(ctx, loc.l("generic.loading_hint"), ephemeral=not public)
 		if not isinstance(_, Message):
 			return
-		checkpoint2 = datetime.fromtimestamp(_.created_at.timestamp()) - checkpoint  # time it took to reply
-		checkpoint = checkpoint - datetime.fromtimestamp(
-		    ctx.id.created_at.timestamp()
-		)  # time it took to receive the command
+
+		reception_latency = start_time - datetime.fromtimestamp(ctx.id.created_at.timestamp(), tz=timezone.utc)
+
+		reply_latency = datetime.fromtimestamp(_.created_at.timestamp(), tz=timezone.utc) - start_time
+
 		host = f"{platform.system()} {platform.release()} ({platform.architecture()[0]})"
-		total_servers = len(ctx.client.guilds)
+
 		team = ctx.client.app.team
 		members = [ctx.client.app.owner]
 		if team:
 			members = [member.user for member in team.members]
+
+		description = ""
+		buttons = []
+		if ctx.client.app.description:
+			for line in ctx.client.app.description.splitlines():
+				try:
+					if ': http' in line and not (len(buttons) > 25):
+						name, url = line.split(':', 1)
+						buttons.append(Button(style=ButtonStyle.LINK, label=name.strip(), url=url.strip()))
+					else:
+						description += "\n" + line
+				except ValueError:
+					description += "\n" + line
+
 		embed = Embed(
-		    description=loc.l(
-		        "misc.stats.layout", owners=" & ".join(map(str, members)), description=ctx.client.app.description
-		    ),
+		    description=loc.l("about.layout", owners=" & ".join(map(str, members)), description=description.strip()),
 		    color=Colors.DEFAULT
 		)
-		embed.add_field(
-		    loc.l("misc.stats.names.avg_ping"),
-		    loc.l("misc.stats.values.time", sec=fnum(ctx.client.latency, ctx.locale)),
-		    inline=True
+		embed.add_fields(
+		    EmbedField(
+		        name=loc.l("about.names.avg_ping"),
+		        value=loc.l("about.values.time", sec=fnum(ctx.client.latency, ctx.locale)),
+		        inline=True
+		    ),
+		    EmbedField(
+		        name=loc.l("about.names.latency"),
+		        value=
+		        f'{loc.l("about.values.time", sec=fnum(reception_latency.microseconds / 1e6, ctx.locale))} / {loc.l("about.values.time", sec=fnum(reply_latency.microseconds / 1e6, ctx.locale))}',
+		        inline=True
+		    ),
+		    EmbedField(
+		        name=loc.l("about.names.cpu_usg"),
+		        value=loc.l("about.values.percent", num=round(psutil.cpu_percent())),
+		        inline=True
+		    ),
+		    EmbedField(
+		        name=loc.l("about.names.mem_usg"),
+		        value=loc.l("about.values.percent", num=round(psutil.virtual_memory().percent)),
+		        inline=True
+		    ), EmbedField(name=loc.l("about.names.server_count"), value=str(ctx.client.guild_count), inline=True),
+		    EmbedField(
+		        name=loc.l("about.names.commit_hash"),
+		        value=commit_hash if commit_hash else loc.l("misc.status.values.failed_commit_hash"),
+		        inline=True
+		    ),
+		    EmbedField(
+		        loc.l("about.names.uptime"), ftime(datetime.now() - ctx.client.start_time, ctx.locale), inline=True
+		    )
 		)
-		embed.add_field(
-		    loc.l("misc.stats.names.latency"),
-		    loc.l("misc.stats.values.time", sec=fnum(checkpoint.microseconds / 1e6, ctx.locale)) + " / " +
-		    loc.l("misc.stats.values.time", sec=fnum(checkpoint2.microseconds / 1e6, ctx.locale)),
-		    inline=True
-		)
-		embed.add_field(
-		    loc.l("misc.stats.names.cpu_usg"),
-		    loc.l("misc.stats.values.percent", num=round(psutil.cpu_percent())),
-		    inline=True
-		)
-		embed.add_field(
-		    loc.l("misc.stats.names.mem_usg"),
-		    loc.l("misc.stats.values.percent", num=round(psutil.virtual_memory().percent)),
-		    inline=True
-		)
-		embed.add_field(loc.l("misc.stats.names.server_count"), total_servers, inline=True)
-		embed.add_field(
-		    loc.l("misc.stats.names.commit_hash"),
-		    commit_hash if commit_hash else loc.l("misc.status.values.failed_commit_hash"),
-		    inline=True
-		)
-		embed.add_field(
-		    loc.l("misc.stats.names.uptime"), ftime(datetime.now() - ctx.client.start_time, ctx.locale), inline=True
-		)
-		# embed.add_field(loc.l("misc.stats.names.user_installs"),
+		# embed.add_field(loc.l("about.names.user_installs"),
 		#                 len(ctx.client.app.users)) # NONEXISTENT
-		embed.add_field(loc.l("misc.stats.names.host"), host, inline=True)
-
-		return await ctx.edit(embeds=[embed])
+		embed.add_field(loc.l("about.names.host"), host, inline=True)
+		rows = []
+		for i in range(0, len(buttons), 5):
+			rows.append(ActionRow(*buttons[i:i + 5]))
+		return await ctx.edit(embeds=[embed], components=rows)
