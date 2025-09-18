9aabbd6d049257ee4e5ffaef53d7b2737933ba22
meqativ <meqativ@users.noreply.github.com>
2025-09-18 04:49:11 +0300

    feat(textbox): try add context menu commands

--- src/extensions/commands/textbox/__init__.py
+++ src/extensions/commands/textbox/__init__.py
@@ -1,7 +1,10 @@
+from typing import Literal
+from utilities.config import get_config
+from utilities.textbox.mediagen import SupportedFiletypes
 from .state import command_ as state_cmd
 from .facepic_selector import init_facepic_selector, handle_facepic_selection
-from interactions import Extension, SlashContext, contexts, integration_types, slash_command
-from .create import command as create_cmd, handle_components as create_handle_components, handle_update_text_modal as create_handle_update_text_modal, handle_edit_modal as create_handle_edit_modal, facepic_autocomplete as create_facepic_autocomplete
+from interactions import Attachment, ContextMenuContext, Extension, Member, Message, OptionType, SlashCommandChoice, SlashContext, User, contexts, integration_types, message_context_menu, slash_command, slash_option, user_context_menu
+from .create import start as start_builder, handle_components as create_handle_components, handle_update_text_modal as create_handle_update_text_modal, handle_edit_modal as create_handle_edit_modal, facepic_autocomplete as create_facepic_autocomplete
 
 
 class TextboxCommands(Extension):
@@ -16,15 +19,119 @@ class TextboxCommands(Extension):
 		"""Base command for textboxes."""
 		pass
 
-	create = textbox.subcommand(
+	@textbox.subcommand(
 	    sub_cmd_name="create",
 	    sub_cmd_description="Make a textbox or a GIF dialogue between characters, like in OneShot"
-	)(create_cmd)
+	)
+	@slash_option(
+	    name='text',
+	    description='What you want the character to say?',
+	    opt_type=OptionType.STRING,
+	    required=False,
+	    max_length=int(get_config("textbox.max-text-length-per-frame", typecheck=int))
+	)
+	@slash_option(
+	    name='facepic',
+	    argument_name='face_path',
+	    description="Which facepic do you want on the textbox? (go back to this option to continue serching)",
+	    opt_type=OptionType.STRING,
+	    required=False,
+	    autocomplete=True,
+	)
+	@slash_option(
+	    name='animated',
+	    description='Do you want the text to animate? (default: true)',
+	    opt_type=OptionType.BOOLEAN,
+	    required=False
+	)
+	@slash_option(
+	    name='force_send',
+	    description="For sending empty textboxes with 'send_to' option (default: false)",
+	    opt_type=OptionType.BOOLEAN,
+	    required=False
+	)
+	@slash_option(
+	    description='What filetype do you want the output to be?',
+	    name="filetype",
+	    opt_type=OptionType.STRING,
+	    choices=[
+	        SlashCommandChoice(name="WEBP (default)", value="WEBP"),
+	        SlashCommandChoice(name="GIF", value="GIF"),
+	        SlashCommandChoice(name="APNG", value="APNG"),
+	        SlashCommandChoice(name="PNG", value="PNG"),
+	        SlashCommandChoice(name="JPEG", value="JPEG")
+	    ]
+	)
+	@slash_option(
+	    name='send_to',
+	    description='Where do you want the output to be sent? (pass all other options for it to render&send right away)',
+	    opt_type=OptionType.INTEGER,
+	    required=False,
+	    choices=[
+	        SlashCommandChoice(name="Don't (default)", value=1),
+	        SlashCommandChoice(name="DMs", value=2),
+	        SlashCommandChoice(name="This channel (here)", value=3)
+	    ]
+	)
+	@slash_option(
+	    argument_name="tbb_file",
+	    name='from_tbb_file',
+	    description='Pass a file here to load a textbox dialogue from a file.tbb (overwrites any other options)',
+	    opt_type=OptionType.ATTACHMENT,
+	    required=False
+	)
+	async def create(
+	    self,
+	    ctx: SlashContext,
+	    text: str = "",
+	    face_path: str | None = None,
+	    force_send: bool | None = False,
+	    animated: bool = True,
+	    tbb_file: Attachment | None = None,
+	    filetype: SupportedFiletypes | None = None,
+	    send_to: Literal[1, 2, 3] = 1
+	):
+		return await start_builder(
+		    self,
+		    ctx=ctx,
+		    text=text,
+		    face_path=face_path,
+		    force_send=force_send,
+		    animated=animated,
+		    tbb_file=tbb_file,
+		    filetype=filetype,
+		    send_to=send_to
+		)
+
 	facepic_autocomplete = create.autocomplete("facepic")(create_facepic_autocomplete)
 	handle_components = create_handle_components
 	handle_update_text_modal = create_handle_update_text_modal
 	handle_edit_modal = create_handle_edit_modal
 
+	@message_context_menu('✏️ convert to textbox')
+	@integration_types(guild=True, user=True)
+	@contexts()
+	async def from_message(self, ctx: ContextMenuContext):
+		assert isinstance(ctx.target, Message), "hi linter"
+		message = ctx.target
+		return await start_builder(
+		    self,
+		    ctx,
+		    message.content,
+		    face_path=message.author.display_avatar.as_url(extension="png", size=96),
+		    force_send=None,
+		    send_to=3
+		)
+
+	@user_context_menu('✏️ use avatar as facepic')
+	@integration_types(guild=True, user=True)
+	@contexts()
+	async def from_user(self, ctx: ContextMenuContext):
+		who = ctx.target
+		assert isinstance(who, (Member, User)), "hi linter"
+		return await start_builder(
+		    self, ctx, face_path=who.display_avatar.as_url(extension="png", size=96), force_send=None, send_to=3
+		)
 	init_facepic_selector = init_facepic_selector
 	handle_facepic_selection = handle_facepic_selection
 
--- src/extensions/commands/textbox/create.py
+++ src/extensions/commands/textbox/create.py
@@ -18,72 +18,15 @@ from utilities.localization import Localization, put_mini
 from utilities.message_decorations import Colors, fancy_message
 from utilities.textbox.mediagen import Frame, SupportedFiletypes, render_textbox_frames
 from utilities.textbox.states import StateShortcutError, State, StateOptions, new_state, state_shortcut
-from interactions import ActionRow, Attachment, AutocompleteContext, Button, ButtonStyle, ComponentContext, Embed, File, MessageFlags, Modal, ModalContext, OptionType, ParagraphText, SlashCommandChoice, SlashContext, component_callback, modal_callback, slash_option, AllowedMentions
+from interactions import ActionRow, Attachment, AutocompleteContext, Button, ButtonStyle, ComponentContext, ContextMenuContext, Embed, File, MessageFlags, Modal, ModalContext, OptionType, ParagraphText, SlashCommandChoice, SlashContext, component_callback, modal_callback, slash_option, AllowedMentions
 
 from utilities.textbox.facepics import f_storage
 nomentions = AllowedMentions(parse=[])
 
 
-@slash_option(
-		name='text',
-		description='What you want the character to say?',
-		opt_type=OptionType.STRING,
-		required=False,
-		max_length=int(get_config("textbox.max-text-length-per-frame", typecheck=int))
-)
-@slash_option(
-		name='facepic',
-		argument_name='face_path',
-		description="Which facepic do you want on the textbox? (go back to this option to continue serching)",
-		opt_type=OptionType.STRING,
-		required=False,
-		autocomplete=True,
-)
-@slash_option(
-		name='animated',
-		description='Do you want the text to animate? (default: true)',
-		opt_type=OptionType.BOOLEAN,
-		required=False
-)
-@slash_option(
-		name='force_send',
-		description="For sending empty textboxes with 'send_to' option (default: false)",
-		opt_type=OptionType.BOOLEAN,
-		required=False
-)
-@slash_option(
-	description='What filetype do you want the output to be?',
-	name="filetype",
-	opt_type=OptionType.STRING,
-	choices=[
-		SlashCommandChoice(name="WEBP (default)", value="WEBP"),
-		SlashCommandChoice(name="GIF", value="GIF"), 
-		SlashCommandChoice(name="APNG", value="APNG"), 
-		SlashCommandChoice(name="PNG", value="PNG"), 
-		SlashCommandChoice(name="JPEG", value="JPEG")
-	]
-)
-@slash_option(
-		name='send_to',
-		description='Where do you want the output to be sent? (pass all other options for it to render&send right away)',
-		opt_type=OptionType.INTEGER,
-		required=False,
-		choices=[
-			SlashCommandChoice(name="Don't (default)", value=1), 
-			SlashCommandChoice(name="DMs", value=2), 
-			SlashCommandChoice(name="This channel (here)", value=3)
-		]
-)
-@slash_option(
-		argument_name="tbb_file",
-		name='from_tbb_file',
-		description='Pass a file here to load a textbox dialogue from a file.tbb (overwrites any other options)',
-		opt_type=OptionType.ATTACHMENT,
-		required=False
-)
-async def command(
+async def start(
 		self, 
-		ctx: SlashContext,
+		ctx: SlashContext | ContextMenuContext,
 		text: str = "",
 		face_path: str | None = None,
 		force_send: bool | None = False,
@@ -130,9 +73,9 @@ async def command(
 			),
 			frames=Frame(text=text)
 		))
-	
-	if force_send or (send_to != 1 and (len(text) != 0 and face_path != None)):
-		await send_output(ctx, state_id, 0)
+	if force_send != None:
+		if force_send or (send_to != 1 and (len(text) != 0 and face_path != None)):
+			await send_output(ctx, state_id, 0)
 
 	await respond(ctx, state_id, 0)
 
@@ -221,7 +164,7 @@ async def handle_components(self, ctx: ComponentContext):
 			
 	await respond(ctx, state_id, int(frame_index))
 
-async def send_output(ctx: ComponentContext | SlashContext, state_id: str, frame_index: int):
+async def send_output(ctx: ComponentContext | SlashContext | ContextMenuContext, state_id: str, frame_index: int):
 	try:
 		loc, state, frame_data = await state_shortcut(ctx, state_id, frame_index)
 	except StateShortcutError:
@@ -266,7 +209,7 @@ async def send_output(ctx: ComponentContext | SlashContext, state_id: str, frame
 			await ctx.edit(message=sent_message, content=f"{sent_message.content}\n{mini}", allowed_mentions=nomentions)
 	return await ctx.edit(message=message, embed=Embed(description=desc, color=Colors.DEFAULT))
 
-async def render_to_file(ctx: ComponentContext|SlashContext|ModalContext, state: State, frame_preview_index: int | None = None) -> File:
+async def render_to_file(ctx: ComponentContext | SlashContext | ModalContext | ContextMenuContext, state: State, frame_preview_index: int | None = None) -> File:
 	loc = Localization(ctx)
 
 	frames = state.frames
@@ -374,7 +317,7 @@ async def handle_edit_modal(self, ctx: ModalContext, updated_frames: str):
 	await respond(ctx, state_id, int(frame_index))
 
 
-async def respond(ctx: SlashContext | ComponentContext | ModalContext, state_id: str | None = None, frame_index: int | None = None, type: Literal['normal', 'loading', 'error']="normal", edit: bool=True, warnings: list = [], error: Any | None = None):
+async def respond(ctx: SlashContext | ComponentContext | ModalContext | ContextMenuContext, state_id: str | None = None, frame_index: int | None = None, type: Literal['normal', 'loading', 'error']="normal", edit: bool=True, warnings: list = [], error: Any | None = None):
 	components = []
 	files = []
 	content: str | None = None
