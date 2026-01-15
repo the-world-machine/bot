12d4e9ac253157ee1476af899cdf46dd6f336f8b
meqativ <meqativ@users.noreply.github.com>
2026-01-16 01:18:29 +0200

    feat(csscolors): add csscolorparser wrapper source code

---
+++ src/utilities/textbox/csscolors/src/main.rs
@@ -0,0 +1,12 @@
+use csscolorparser;
+use std::env;
+fn main() {
+    env::args().skip(1).for_each(|arg| {
+        let parsed = csscolorparser::parse(&arg);
+        if let Err(err) = parsed {
+            eprintln!("Error: {}", err);
+            return;
+        }
+        println!("{:?}", parsed.unwrap().to_rgba8());
+    });
+}
